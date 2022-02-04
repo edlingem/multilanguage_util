9131d8d84b25e9a7857aaa8f8dcc2195ce146402
Forumat <forumat.kontakt@gmail.com>
2022-02-04 18:14:51 +0100

    set up rest module

---
+++ rest/src/main/java/at/marc_edlinger/multilang/MultiLangRestApp.java
@@ -0,0 +1,13 @@
+package at.marc_edlinger.multilang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLangRestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiLangRestApp.class, args);
+    }
+
+}

30f7a7fbe012d4187322795618404aa6811afe44
Forumat <forumat.kontakt@gmail.com>
2022-02-04 18:38:52 +0100

    added fist example in LanguageRestController, and fixed generic bug in LanguageRepository

--- rest/src/main/java/at/marc_edlinger/multilang/language/LanguageRepository.java
+++ rest/src/main/java/at/marc_edlinger/multilang/language/LanguageRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LanguageRepository extends JpaRepository<String, Language> {}
+public interface LanguageRepository extends JpaRepository<Language, String> {}
--- rest/src/main/java/at/marc_edlinger/multilang/language/LanguageRestController.java
+++ rest/src/main/java/at/marc_edlinger/multilang/language/LanguageRestController.java
@@ -1,17 +1,30 @@
 package at.marc_edlinger.multilang.language;
 
+import at.marc_edlinger.multilang.config.KeyValidator;
+import at.marc_edlinger.multilang.exception.RestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(
-        path = "api/v1/lang/"
+        path = "api/v1/lang/{key}/"
 )
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class LanguageRestController {
 
     private final LanguageRepository repository;
+
+    @GetMapping("fetch/all")
+    public List<Language> getLanguages(@PathVariable String key) throws RestException {
+        KeyValidator.validate(key);
+
+        return repository.findAll();
+    }
 
 }
